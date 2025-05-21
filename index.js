89cd425742705e6f18609780984d5b62a10a7ff4
VIGNESH KARIMIKONDA <vigneshkarimikonda@VIGNESHs-MacBook-Air.local>
2025-05-22 05:23:05 +0530

    allowed all routes to access

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = 5001;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 let db;
